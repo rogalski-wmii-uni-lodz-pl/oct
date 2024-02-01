b624780519766542e527f0e5409fc1257cbe20d6
Mark Rogalski <marek.rogalski@wmii.uni.lodz.pl>
2024-02-01 21:16:22 +0100

    Rename Data to Octal, add counts and occasional redoing_common

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{cmp::Reverse, collections::HashSet};
 
 use bitvec::prelude::*;
 
@@ -11,8 +11,8 @@ pub fn to_nimpos(x: Nimber, p: usize) -> Nimpos {
     (x << 1) | (p & 1)
 }
 
-pub fn from_nimpos(x: Nimpos) -> (Nimber, bool) {
-    (x >> 1, x & 1 == 1)
+pub fn from_nimpos(x: Nimpos) -> (Nimber, usize) {
+    (x >> 1, x & 1)
 }
 
 pub fn xor(x: Nimpos, y: Nimpos, d: usize) -> Nimpos {
@@ -65,7 +65,9 @@ impl Bin {
 pub struct Rules {
     pub all: Vec<usize>,
     pub some: Vec<usize>,
-    pub divide: Vec<usize>,
+    pub divide_all: Vec<usize>,
+    pub divide: [Vec<usize>; 2],
+    pub len: usize,
 }
 
 /// Transform a game string like "0.034" into Rules
@@ -76,10 +78,16 @@ pub fn rules_from_str(game: &str) -> Rules {
         .map(|x| x.to_digit(10).unwrap() as usize)
         .collect_vec();
 
+    let divide_all = extract_bit(&vals, 2);
+    let (even, odd): (Vec<usize>, Vec<usize>) =
+        divide_all.iter().partition(|&x| x & 1 == 0);
+
     Rules {
         all: extract_bit(&vals, 0),
         some: extract_bit(&vals, 1),
-        divide: extract_bit(&vals, 2),
+        divide_all,
+        divide: [even, odd],
+        len: vals.len() - 1,
     }
 }
 
@@ -97,40 +105,15 @@ fn extract_bit(vals: &Vec<usize>, b: usize) -> Vec<usize> {
         .collect_vec()
 }
 
-pub fn def(n: usize, rules: &Rules, g: &Vec<Nimber>, largest: usize) -> usize {
-    let mut seen = Bin::make(largest);
-    for &d in rules.all.iter() {
-        if n == d {
-            seen.set_bit(0);
-        }
-    }
-
-    for &d in rules.some.iter() {
-        if n > d {
-            seen.set_bit(g[n - d]);
-        }
-    }
-
-    for &d in rules.divide.iter() {
-        if n > d {
-            for i in 1..=(n - d) / 2 {
-                seen.set_bit(g[i] ^ g[n - d - i]);
-            }
-        }
-    }
-
-    seen.lowest_unset()
-}
-
 fn can_add_to_common(common: &HashSet<usize>, np: Nimpos, parity: usize) -> bool {
     // 0.142
     // if np == 3 || np == 257 || np == 512 {
     //     return false;
     // }
     // 0.104
-    if np == 8 {
-        return false;
-    }
+    // if np == 8 {
+    //     return false;
+    // }
     let with_itself = xor(np, np, parity);
     if with_itself == np || common.contains(&with_itself) {
         return false;
@@ -146,25 +129,103 @@ fn can_add_to_common(common: &HashSet<usize>, np: Nimpos, parity: usize) -> bool
 }
 
 #[derive(Debug)]
-pub struct Data {
+pub struct Octal {
     pub common: [HashSet<usize>; 2],
     pub both_common: Bin,
     pub rares: [Vec<(usize, Nimber)>; 2],
     pub largest: usize,
+    pub last_divide: usize,
     pub even: bool,
     pub odd: bool,
+    pub g: Vec<Nimber>,
+    pub rules: Rules,
+    pub counts: Vec<usize>,
+    pub seen: Bin,
 }
 
-impl Data {
-    pub fn new(largest: usize, rules: &Rules) -> Self {
+const CHECK: bool = false;
+
+impl Octal {
+    pub fn new(g_size: usize, largest: usize, rules: Rules) -> Self {
         Self {
             both_common: Bin::make(2 * largest),
             common: [HashSet::new(), HashSet::new()],
             rares: [vec![], vec![]],
             largest,
-            even: rules.divide.iter().any(|x| x & 1 == 0),
-            odd: rules.divide.iter().any(|x| x & 1 == 1),
+            last_divide: *rules.divide_all.last().unwrap(),
+            even: rules.divide[0].len() != 0,
+            odd: rules.divide[1].len() != 0,
+            rules,
+            g: vec![0; g_size],
+            counts: vec![0; largest << 2 + 1],
+            seen: Bin::make(largest),
         }
+    }
+
+    pub fn calc(self: &mut Self, n: usize) {
+        let gn = if n > self.rules.len {
+            if CHECK {
+                let check = self.def(n);
+                let gn = self.rc(n);
+                dbg!(n, gn, check);
+                assert!(gn == check);
+                gn
+            } else {
+                self.rc(n)
+            }
+        } else {
+            self.def(n)
+        };
+
+        if self.largest < gn {
+            self.largest = gn;
+            self.resize(self.largest);
+        }
+
+        self.counts[to_nimpos(gn, n)] += 1;
+
+        self.add_to_both_common_or_rare(n, gn);
+
+        self.g[n] = gn;
+
+        if n.is_power_of_two() {
+            self.redo_common(n);
+        }
+
+        println!(
+            "n: {n} val: {gn} {} {}",
+            &self.rares[0].len(),
+            &self.rares[1].len()
+        );
+        if CHECK {
+            dbg!(&self.common);
+            dbg!(&self.rares);
+        }
+    }
+
+    pub fn def(self: &mut Self, n: usize) -> usize {
+        let mut seen = Bin::make(self.largest);
+        for &d in self.rules.all.iter() {
+            if n == d {
+                seen.set_bit(0);
+            }
+        }
+
+        for &d in self.rules.some.iter() {
+            if n > d {
+                seen.set_bit(self.g[n - d]);
+            }
+        }
+
+        for &d in self.rules.divide_all.iter() {
+            if n > d {
+                for i in 1..=(n - d) / 2 {
+                    seen.set_bit(self.g[i] ^ self.g[n - d - i]);
+                }
+            }
+        }
+
+        seen.lowest_unset()
     }
 
     pub fn resize(self: &mut Self, largest: usize) {
@@ -172,24 +233,66 @@ impl Data {
         common2.set_all_bits_from(&self.both_common);
         self.both_common = common2;
         self.largest = largest;
+        self.counts.resize(largest << 2 + 1, 0);
+        self.seen = Bin::make(largest);
+        // self.redo_common();
     }
 
-    pub fn add_to_common(self: &mut Self, n: usize, gn: Nimber, parity: usize) -> bool {
-        let np = to_nimpos(gn, n);
+    pub fn redo_common(self: &mut Self, n: usize) {
+        self.both_common = Bin::make(2 * self.largest);
+        self.common[0].clear();
+        self.common[1].clear();
+        self.rares[0].clear();
+        self.rares[1].clear();
+
+        // this clone here is probably bad :(
+        for (np, count) in self.counts.clone().iter().enumerate().sorted_by_key(|&(n, count)| (Reverse(count), n)) {
+            if *count > 0 {
+                let (n, p) = from_nimpos(np);
+                println!("{n} {p} {count}");
+                self.add_to_both_common(np);
+            }
+        }
+
+        for i in 1..=n {
+            let np = to_nimpos(self.g[i], i);
+            if self.even && !self.common[0].contains(&np) {
+                self.rares[0].push((i, self.g[i]));
+            }
+            if self.odd && !self.common[1].contains(&np) {
+                self.rares[1].push((i, self.g[i]));
+            }
+        }
+        self.rares[0].sort();
+        self.rares[1].sort();
+
+    }
+
+    pub fn add_to_common(self: &mut Self, np: Nimpos, parity: usize) -> bool {
         if self.common[parity].contains(&np) {
             return true;
         }
 
-        let not_zero = match (self.even, self.odd) {
-            (true, true) => np != parity,
-            (true, false) => np != 0,
-            (false, true) => np != 1,
-            (false, false) => false, // who cares?
-        };
+        // let not_zero = match (self.even, self.odd) {
+        //     (true, true) => np != parity,
+        //     (true, false) => np != 0,
+        //     (false, true) => np != 1,
+        //     (false, false) => false, // who cares?
+        // };
+        //
+        let not_zero = np != parity;
 
         if not_zero && can_add_to_common(&self.common[parity], np, parity) {
             self.common[parity].insert(np);
-            // self.both_common.set_bit(np);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn add_to_common_or_rare(self: &mut Self, n: usize, gn: Nimber, parity: usize) -> bool {
+        let np = to_nimpos(gn, n);
+        if self.add_to_common(np, parity) {
             true
         } else {
             self.rares[parity].push((n, gn));
@@ -197,14 +300,32 @@ impl Data {
         }
     }
 
-    pub fn add_to_common2(self: &mut Self, n: usize, gn: Nimber) {
+    pub fn add_to_both_common(self: &mut Self, np: Nimpos) {
         let ec = if self.even {
-            self.add_to_common(n, gn, 0)
+            self.add_to_common(np, 0)
         } else {
             true
         };
         let oc = if self.odd {
-            self.add_to_common(n, gn, 1)
+            self.add_to_common(np, 1)
+        } else {
+            true
+        };
+
+        if ec && oc {
+            self.both_common.set_bit(np);
+        }
+    }
+
+
+    pub fn add_to_both_common_or_rare(self: &mut Self, n: usize, gn: Nimber) {
+        let ec = if self.even {
+            self.add_to_common_or_rare(n, gn, 0)
+        } else {
+            true
+        };
+        let oc = if self.odd {
+            self.add_to_common_or_rare(n, gn, 1)
         } else {
             true
         };
@@ -214,111 +335,86 @@ impl Data {
             self.both_common.set_bit(np);
         }
     }
-}
 
-pub fn rc(n: usize, rules: &Rules, g: &Vec<Nimber>, data: &Data) -> usize {
-    let mut seen = Bin::make(data.largest);
+    pub fn rc(self: &mut Self, n: usize) -> usize {
+        self.seen.zero_bits();
 
-    // set some
-    for d in rules.some.iter() {
-        let x = g[n - d];
-        seen.set_bit(x);
+        self.set_some(n);
+        self.set_0_if_divisible_into_same_size(n);
+        self.set_rare(n);
+        self.prove(n)
     }
 
-    if (n & 1 == 0 && data.even) || (n & 1 == 1 && data.odd) {
-        seen.set_bit(0);
-    }
-
-    // set rare
-    for &d in rules.divide.iter() {
-        for &(i, r) in data.rares[d & 1].iter() {
-            if n - d > i {
-                let gndi = g[n - d - i];
-                let x = r ^ gndi;
-                seen.set_bit(x);
-            }
-        }
-    }
-
-    let mut m = seen.lowest_unset();
-
-    let mp = to_nimpos(m, n);
-
-    if data.both_common.get(mp) {
-        return m;
-    }
-
-    for i in 1..(n - rules.divide.last().unwrap()) {
-        for d in rules.divide.iter() {
-            let gndi = g[n - d - i];
-            let gi = g[i];
-
-            let x = gi ^ gndi;
-
-            seen.set_bit(x);
-        }
-
-        m = seen.lowest_unset();
+    fn prove(self: &mut Self, n: usize) -> usize {
+        let mut m = self.seen.lowest_unset();
 
         let mp = to_nimpos(m, n);
-        if data.both_common.get(mp) {
+
+        if self.both_common.get(mp) {
             return m;
         }
-    }
-    for i in n - rules.divide.last().unwrap()..n {
-        for &d in rules.divide.iter() {
-            if n - d > i {
-                let gndi = g[n - d - i];
-                let gi = g[i];
+
+        for i in 1..(n - self.last_divide) {
+            for d in self.rules.divide_all.iter() {
+                let gndi = self.g[n - d - i];
+                let gi = self.g[i];
 
                 let x = gi ^ gndi;
 
-                seen.set_bit(x);
+                self.seen.set_bit(x);
+            }
+
+            m = self.seen.lowest_unset();
+
+            let mp = to_nimpos(m, n);
+            if self.both_common.get(mp) {
+                return m;
+            }
+        }
+        for i in n - self.last_divide..n {
+            for &d in self.rules.divide_all.iter() {
+                if n - d > i {
+                    let gndi = self.g[n - d - i];
+                    let gi = self.g[i];
+
+                    let x = gi ^ gndi;
+
+                    self.seen.set_bit(x);
+                }
+            }
+        }
+
+        self.seen.lowest_unset()
+    }
+
+    fn set_rare(self: &mut Self, n: usize) {
+        // set rare
+        for parity in [0, 1] {
+            for &d in self.rules.divide[parity].iter() {
+                for &(i, r) in self.rares[parity].iter() {
+                    if n - d > i {
+                        let gndi = self.g[n - d - i];
+                        let x = r ^ gndi;
+                        self.seen.set_bit(x);
+                    }
+                }
             }
         }
     }
 
-    seen.lowest_unset()
+    fn set_0_if_divisible_into_same_size(self: &mut Self, n: usize) {
+        if (n & 1 == 0 && self.even) || (n & 1 == 1 && self.odd) {
+            self.seen.set_bit(0);
+        }
+    }
 
-    // if data.even && data.odd {
-    //     assert!(m[0] == m[1]);
-    //     return m[0];
-    // } else if data.even {
-    //     return m[0];
-    // } else if data.odd {
-    //     return m[1];
-    // }
-    // return seen.;
+    fn set_some(self: &mut Self, n: usize) {
+        for d in self.rules.some.iter() {
+            let x = self.g[n - d];
+            self.seen.set_bit(x);
+        }
+    }
 }
-
-// pub fn rc2(
-//     n: usize,
-//     rules: &Rules,
-//     g: &Vec<Nimber>,
-//     rares: &Vec<(usize, Nimber)>,
-//     rare: &Bin,
-//     largest: Nimber,
-// ) {
-//     let mut seen = Bin::make(largest);
-
-//     // set some
-//     for d in rules.some.iter() {
-//         seen.set_bit(g[n - d]);
-//     }
-
-//     // set rare
-//     for (i, r) in rares.iter() {
-//         for d in rules.divide.iter() {
-//             seen.set_bit(r ^ g[n - d - i]);
-//         }
-//     }
-
-//     let first_common = seen.find_first_unset_also_unset_in(&rare);
-
-//     let mut mex = seen.copy_up_to_inclusive(first_common + 1);
-
-//     let mut remaining_unset = mex.count_unset() - 1; // -1 for mex[first_common]
-// }
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use oct;
 
-const CHECK: bool = false;
-
 fn main() {
     // let rules = oct::rules_from_str("0.142");
     // let rules = oct::rules_from_str("0.104"); // breaks down for 12: (4, 0)
@@ -10,54 +8,30 @@ fn main() {
     // let game = "0.205";
     // let game = "0.142";
     // let game = "0.051";
-    let game = "0.104";
+    // let game = "0.104";
     // let game = "0.106";
     // let game = "0.051";
-    // let game = "0.165";
-    let rules = oct::rules_from_str(game); // breaks down for (2, 1) and (45, 8)
-
-    // let mut g = vec![0; 1_000_000];
-    let mut g = vec![0; 1_000_000];
-
-    let mut largest = 1;
-
-    let mut data = oct::Data::new(largest, &rules);
+    let game = "0.166";
+    let rules = oct::rules_from_str(game);
     dbg!(&rules);
 
-    for n in 1..g.len() {
-        let gn = if n > game.len() {
-            if CHECK {
-                let check = oct::def(n, &rules, &g, largest);
-                let gn = oct::rc(n, &rules, &g, &data);
-                dbg!(n, gn, check);
-                assert!(gn == check);
-                gn
-            } else {
-                oct::rc(n, &rules, &g, &data)
-            }
-        } else {
-            oct::def(n, &rules, &g, largest)
-        };
+    // let mut g = vec![0; 1_000_000];
+    let mut octal = oct::Octal::new(1_000_000, 1, rules);
 
-        if largest < gn {
-            largest = gn;
-            data.resize(largest);
-        }
+    for n in 1..octal.g.len() {
+        octal.calc(n);
+    }
+    // dbg!(&octal);
 
-        data.add_to_common2(n, gn);
-
-        g[n] = gn;
-
-        println!(
-            "n: {n} val: {gn} {} {}",
-            &data.rares[0].len(),
-            &data.rares[1].len()
-        );
-        if CHECK {
-            dbg!(&data);
+    for (n, &count) in octal.counts.iter().enumerate() {
+        if count != 0 {
+            println!(
+                "{n}: {count} {} {}",
+                octal.rares[0].contains(&oct::from_nimpos(n)),
+                octal.rares[1].contains(&oct::from_nimpos(n))
+            );
         }
     }
-    dbg!(&data);
 
     // dbg!(&data.common_bitset.len());
 }
